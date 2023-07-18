ed6a2a235d53a5ae7239696afcd66757beac2945
Maalxi <maalxi38@gmail.com>
2023-07-18 15:12:50 +0200

    Mise à jour le 18-07-2023 - ajout de l'api

--- front/src/app/page.jsx
+++ front/src/app/page.jsx
@@ -1,95 +1,46 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./pageAccueil.module.css";
+import js_navbar from "./pageAccueil.module.css";
+import css_navbar from "./pageAccueil.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <nav>
+        <a href="/" class="nav-icon">
+          <Image
+            className={styles.logo}
+            src="/image/Cerise.png"
+            alt="Cherry icon"
+            width={60}
+            height={60}
+          />
+        </a>
+
+        <div class="main-navlinks">
+          <button
+            type="button"
+            class="hamburger"
+            aria-label="Toggle Navigation"
+            aria-expanded="false"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+
+          <div class="navlinks-container">
+            <a href="#">Accueil</a>
+            <a href="#">Produits</a>
+            <a href="#">Panier</a>
+            <a href="#">Contact</a>
+            {/* <a href="#">Sponsors</a> */}
+            {/* <a href="#">Inscriptions</a> */}
+          </div>
         </div>
-      </div>
+      </nav>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <h2>test</h2>
+    </>
+  );
 }
