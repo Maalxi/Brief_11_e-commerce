516d9be20afc5dd16f30759da60bd2b2ca8b3e9c
Maalxi <maalxi38@gmail.com>
2023-07-19 16:16:14 +0200

    Mise à jour le 19-07-2023 - update

--- front/src/app/Layout.jsx
+++ front/src/app/Layout.jsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import Menu from "./components/Navbar/Navbar";
-import Footer from "./components/Navbar/Footer/Footer";
+import Footer from "./components/Footer/Footer";
 
 export default function Layout({ children }) {
   return (
