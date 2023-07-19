7f6e25660d03655fc5b5f7b997a4aefee610d239
Maalxi <maalxi38@gmail.com>
2023-07-19 10:17:06 +0200

    Mise à jour le 19-07-2023 - ajout Navbar

---
+++ front/src/app/page.js
@@ -0,0 +1,14 @@
+"use client"
+
+import Image from "next/image";
+import styles from "./pageAccueil.module.css";
+
+import Layout from "./Layout.jsx";
+
+export default function Home() {
+  return (
+    <Layout>
+
+      </Layout>
+  );
+}
