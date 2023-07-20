943ac12031f4886ecd4eb879719e5b1ff97e13ce
Guillaume <estrade.guillaume.simplon@gmail.com>
2023-07-20 16:50:35 +0200

    navBar reloaded

---
+++ front/src/app/components/Navbar/Navbar copy.js
@@ -0,0 +1,32 @@
+// import React from 'react';
+// import './Navbar.css';
+// import Link from 'next/link';
+
+// /////// ICONS ///////
+// import { LuCherry } from 'react-icons/lu';
+// import { FaListUl } from 'react-icons/fa';
+// import { RiShoppingBasket2Line } from 'react-icons/ri';
+// import { BsEnvelope } from 'react-icons/bs';
+
+
+// const Navbar = () => {
+//   return (
+//     <div className='navbarContainer'>
+
+//       <Link href='/'>
+//         <img className='imgLogoHeader' src="/image/Cerise.png" alt="Logo Épicerise" />
+//       </Link>
+
+
+//       <ul className="navbarContainerLinks">
+//         <li className="navbarContainerLi"><Link className="navbarContainerLink" href="/"><LuCherry /> L’ÉpiCerise</Link></li>
+//         <li className="navbarContainerLi"><Link className="navbarContainerLink" href="#"><FaListUl /> Produits</Link></li>
+//         <li className="navbarContainerLi"><Link className="navbarContainerLink" href="#"><RiShoppingBasket2Line /> Panier</Link></li>
+//         <li className="navbarContainerLi"><Link className="navbarContainerLink" href="#"><BsEnvelope /> Contact</Link></li>
+//       </ul>
+
+//     </div >
+//   );
+// }
+
+// export default Navbar;
--- front/src/app/components/Navbar/Navbar.js
+++ front/src/app/components/Navbar/Navbar.js
@@ -1,18 +1,82 @@
+"use client"
 import React from 'react';
-import './Navbar.css';
+import styles from './Navbar.module.css'
+import Link from 'next/link';
 
-const Navbar = () => {
+/////// ICONS ///////
+import { LuCherry } from 'react-icons/lu';
+import { FaListUl } from 'react-icons/fa';
+import { RiShoppingBasket2Line } from 'react-icons/ri';
+import { BsEnvelope } from 'react-icons/bs';
+
+import { useState } from "react";
+
+
+export default function Navbar() {
+  const [selectedMenu, setSelectedMenu] = useState();
+
+  const menuLinks = [
+    {
+      href: "/",
+      label: (
+        <>
+          <LuCherry /> L'ÉpiCerise
+        </>
+      )
+    },
+    {
+      href: "/#",
+      label: (
+        <>
+          <FaListUl /> Produits
+        </>
+      )
+    },
+    {
+      href: "/#",
+      label: (
+        <>
+          <RiShoppingBasket2Line /> Panier
+        </>
+      )
+    },
+    {
+      href: "/#",
+      label: (
+        <>
+          <BsEnvelope /> Contact
+        </>
+      )
+    },
+  ]
+
+  const handleMenuSelection = (index) => {
+    console.log(index)
+    setSelectedMenu(index)
+  }
+
+  const handleLogoClick = () => {
+    setSelectedMenu(null);
+  };
+
   return (
-    <div>
-      <ul className="nav">
-        <li className="nav-item slam-left"><a className="hoverLogo" href="/"><img className="logo" src="/image/Cerise.png" alt="Logo de l'épicerise" /></a></li>
-        <li className="nav-item"><a href="#">L’ÉpiCerise</a></li>
-        <li className="nav-item"><a href="#">Produits</a></li>
-        <li className="nav-item"><a href="#">Panier</a></li>
-        <li className="nav-item"><a className="contact" href="#">Contact</a></li>
+    <nav className={styles.navbarContainer}>
+      <Link href='/'>
+        <img className={styles.imgLogoHeader} src="/image/Cerise.png" alt="Logo Épicerise" />
+      </Link>
+
+      <ul className={styles.navbarContainerLinks}>
+        {menuLinks.map((menu, index) => (
+          <li className={styles.navbarContainerLi} key={index} onClick={() => handleMenuSelection(index)}>
+            <Link
+              className={`${styles.navbarContainerLink} ${selectedMenu === index ? styles.selectedMenu : ""}`}
+              href={menu.href}
+            >
+              {menu.label}
+            </Link>
+          </li>
+        ))}
       </ul>
-    </div>
+    </nav>
   );
 }
-
-export default Navbar;
