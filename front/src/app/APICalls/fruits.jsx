843d9ae895d3d2047752bb09260b8c39f961faeb
Maalxi <maalxi38@gmail.com>
2023-07-21 11:49:18 +0200

    Mise à jour le 21-07-2023 - update

--- front/src/app/APICalls/fruits.jsx
+++ front/src/app/APICalls/fruits.jsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
 import { useState, useEffect } from "react";
 
 const getFruits = async () => {
-  const fruitsData = await fetch("http://localhost:8001/api/products");
+const fruitsData = await fetch("https://localhost:8001/api/products");
 
   if (!fruitsData.ok) {
     throw new Error("Ne peut pas récupérer les fruits");
@@ -16,12 +16,15 @@ const FruitsList = () => {
   const [fruits, setFruits] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const data = await getFruits();
-      setFruits(data.results);
-    };
+    // const fetchData = async () => {
+      // const fruitsData = await fetch("http://127.0.0.1:8001/api/products");
+      // const data = await getFruits();
+      // setFruits(data.results);
+      getFruits();
+      // console.log(fruitsData);
+    // };
 
-    fetchData();
+    // fetchData();
   }, []);
 
   return (
