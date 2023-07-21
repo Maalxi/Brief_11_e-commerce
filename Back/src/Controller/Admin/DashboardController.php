1b5877e687ceab7c6b4b5d9ab429ff21876877c1
Vela <davidbak38@gmail.com>
2023-07-21 11:15:32 +0200

    EazyAdmin terminé

--- Back/src/Controller/Admin/DashboardController.php
+++ Back/src/Controller/Admin/DashboardController.php
@@ -2,45 +2,55 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Entity\Promotion;
+use App\Entity\Reservation;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        // return parent::index();
 
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(CategoryCrudController::class)->generateUrl());
 
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Back');
+            ->setTitle('EpiCerise');
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        // SECTION
+        yield MenuItem::section('Boutiques');
+
+        // LIEN VERS PAGE DE CRUD
+        yield MenuItem::linkToCrud('Catégories', 'fa-solid fa-clipboard-list', Category::class);
+        yield MenuItem::linkToCrud('Produits', 'fa-solid fa-list', Product::class);
+        yield MenuItem::linkToCrud('Promotions', 'fa-solid fa-gift', Promotion::class);
+
+
+        // SECTION
+        yield MenuItem::section('Services');
+
+        // LIEN VERS PAGE DE CRUD
+        yield MenuItem::linkToCrud('Réservations', 'fa-solid fa-basket-shopping', Reservation::class);
+        yield MenuItem::linkToCrud('Client', 'fa-solid fa-user', User::class);
+
+
     }
 }
