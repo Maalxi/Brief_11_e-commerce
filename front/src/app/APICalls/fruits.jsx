024b946683bf114453f4e8aa0a435e174ff4d2c2
Vela <davidbak38@gmail.com>
2023-07-21 11:50:35 +0200

    ajout corp fetch

--- front/src/app/APICalls/fruits.jsx
+++ front/src/app/APICalls/fruits.jsx
@@ -1,9 +1,16 @@
-"use client"
+"use client";
 
 import { useState, useEffect } from "react";
 
 const getFruits = async () => {
-  const fruitsData = await fetch("http://localhost:8001/api/products");
+  try {
+    const fruitsData = await fetch("http://localhost:8001/api/products");
+    const data = await fruitsData.json()
+    console.log(data["hydra:member"]);
+  } catch (error) {
+    console.error(error)
+  }
+
 
   if (!fruitsData.ok) {
     throw new Error("Ne peut pas récupérer les fruits");
@@ -16,12 +23,15 @@ const FruitsList = () => {
   const [fruits, setFruits] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const data = await getFruits();
-      setFruits(data.results);
-    };
+    // const fetchData = async () => {
+      // const fruitsData = await fetch("http://127.0.0.1:8001/api/products%22);
+      // const data = await getFruits();
+      // setFruits(data.results);
+      getFruits();
+      // console.log(fruitsData);
+    // };
 
-    fetchData();
+    // fetchData();
   }, []);
 
   return (
