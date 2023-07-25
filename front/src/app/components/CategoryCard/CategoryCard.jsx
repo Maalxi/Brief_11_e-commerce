055f8e43f86096947569d0439ca3c98d4bc8e2a9
Vela <davidbak38@gmail.com>
2023-07-25 16:47:00 +0200

    gestion du filte / filtre from acceuil

--- front/src/app/components/CategoryCard/CategoryCard.jsx
+++ front/src/app/components/CategoryCard/CategoryCard.jsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import './CategoryCard.css';
 import Link from "next/link";
-
 import { base_url } from "@/app/APICalls/base_url";
 
-export default function CategoryCard({ image, name, key }) { // Utilisez des accolades pour déstructurer les props
-    const img_url = base_url + "127.0.0.1:8001/uploads/images/" + image; // Construisez correctement l'URL
 
+export default function CategoryCard({ image, name, key, cat_id }) { // Utilisez des accolades pour déstructurer les props
+    const img_url = base_url + "127.0.0.1:8001/uploads/images/" + image; // Construisez correctement l'URL
+    const lien = base_url + "localhost:8000/produits?category=" + cat_id
     return (
         <>
-            <Link className="CategoryCardLink" href='#'>
+            <Link className="CategoryCardLink" href={lien} >
                 <div className="CategoryCardContainer">
 
                     <Image className="CategoryCardImg"
---
+++ front/src/app/components/CategoryCard/CategoryCardfilter.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import './CategoryCard.css';
+
+import { base_url } from "@/app/APICalls/base_url";
+
+export default function CategoryCardFilter({ image, name, key }) { // Utilisez des accolades pour déstructurer les props
+    const img_url = base_url + "127.0.0.1:8001/uploads/images/" + image; // Construisez correctement l'URL
+
+    return (
+        <>
+            <div className="CategoryCardLink">
+            <div className="CategoryCardContainer">
+                <Image className="CategoryCardImg"
+                    src={img_url} // Utilisez img_url pour l'attribut src de l'Image
+                    alt={key}
+                    width={80}
+                    height={80}
+                />
+                <p className="CategoryCardTitle">{name}</p>
+            </div>
+        </div >
+        </>
+    );
+}
