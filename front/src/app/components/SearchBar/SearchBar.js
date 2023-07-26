ae2064fb018dd2d943e23dc4fdb43cedae3e4aba
Maalxi <maalxi38@gmail.com>
2023-07-26 15:12:46 +0200

    Mise à jour le 26-07-2023 - update

--- front/src/app/components/SearchBar/SearchBar.js
+++ front/src/app/components/SearchBar/SearchBar.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
-import './SearchBar.css';
-import { ImSearch } from 'react-icons/im';
+import React, { useState } from "react";
+import "./SearchBar.css";
+import { ImSearch } from "react-icons/im";
 
-import { base_url } from '@/app/APICalls/base_url';
+import ProduitCard from "../ProduitCard/ProduitCard";
 
-const suggestions = ['Fruits', 'carotte', 'lait', 'savon', 'poulet'];
+import { base_url } from "@/app/APICalls/base_url";
+
+const suggestions = ["Fruits", "carotte", "lait", "savon", "poulet"];
 
 export default function SearchBar() {
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
   const [prodData, setProdData] = useState([]);
 
   const img_url = "http://127.0.0.1:8001/uploads/images/";
@@ -16,12 +18,14 @@ export default function SearchBar() {
     try {
       const response = await fetch(`${base_url}127.0.0.1:8001/api/products`);
       const data = await response.json();
-      const filteredData = data["hydra:member"].filter(product => product.name.toLowerCase().includes(searchValue.toLowerCase()));
+      const filteredData = data["hydra:member"].filter((product) =>
+        product.name.toLowerCase().includes(searchValue.toLowerCase())
+      );
       setProdData(filteredData); // Stockez les données filtrées dans le state
     } catch (error) {
       console.error(error);
     }
-};
+  };
 
   function handleSearchChange(event) {
     setSearchValue(event.target.value);
@@ -41,7 +45,7 @@ export default function SearchBar() {
           className="searchInput"
           value={searchValue}
           onChange={handleSearchChange}
-          placeholder={suggestions.join(', ')}
+          placeholder={suggestions.join(", ")}
         />
         <button type="submit" className="searchBtn">
           <ImSearch />
@@ -51,14 +55,18 @@ export default function SearchBar() {
       {/* Afficher les résultats de la recherche */}
       {prodData.length > 0 && (
         <div className="searchResults">
-          {prodData.map((product, index) => (
-            <div key={index} className="productItem">
-              <h3>{product.name}</h3>
-              <p>{product.description}</p>
-              <p>Prix: {product.price}€</p>
-              <img className='ImgSearch' src={`${img_url}${product.image}`} alt={product.name} />
-            </div>
-          ))}
+          <div>
+            {prodData.map((item, index) => (
+              <ProduitCard
+                key={index + 1}
+                name={item.name}
+                desc={item.description}
+                price={item.price}
+                image={item.image}
+                stock={item.inventory}
+              />
+            ))}
+          </div>
         </div>
       )}
     </div>
