7d1301daf6d6d86d3075e0427c749e9495f4bee3
Maalxi <maalxi38@gmail.com>
2023-07-26 15:05:18 +0200

    Mise à jour le 26-07-2023 - update

--- front/src/app/APICalls/base_url.js
+++ front/src/app/APICalls/base_url.js
@@ -1,1 +1,1 @@
-export const base_url = "http://"
+export const base_url = "https://"
--- front/src/app/components/SearchBar/SearchBar.js
+++ front/src/app/components/SearchBar/SearchBar.js
@@ -1,41 +1,66 @@
 import React, { useState } from 'react';
 import './SearchBar.css';
-
 import { ImSearch } from 'react-icons/im';
 
-
+import { base_url } from '@/app/APICalls/base_url';
 
 const suggestions = ['Fruits', 'carotte', 'lait', 'savon', 'poulet'];
 
 export default function SearchBar() {
   const [searchValue, setSearchValue] = useState('');
+  const [prodData, setProdData] = useState([]);
 
-  // Fonction pour gérer le changement de valeur de l'input de recherche
+  const img_url = "http://127.0.0.1:8001/uploads/images/";
+
+  const fetchDataProd = async (searchValue) => {
+    try {
+      const response = await fetch(`${base_url}127.0.0.1:8001/api/products`);
+      const data = await response.json();
+      const filteredData = data["hydra:member"].filter(product => product.name.toLowerCase().includes(searchValue.toLowerCase()));
+      setProdData(filteredData); // Stockez les données filtrées dans le state
+    } catch (error) {
+      console.error(error);
+    }
+};
+
   function handleSearchChange(event) {
     setSearchValue(event.target.value);
   }
 
-  // Fonction pour gérer la soumission de la recherche
   function handleSearchSubmit(event) {
     event.preventDefault();
-    // Ici, vous pouvez ajouter la logique pour traiter la recherche, par exemple, envoyer la requête à une API, etc.
-    console.log('Recherche soumise :', searchValue);
+    fetchDataProd(searchValue);
   }
 
   return (
-    <form className="searchForm" onSubmit={handleSearchSubmit}>
-      <input
-        type="text"
-        id="searchInput"
-        className="searchInput"
-        value={searchValue}
-        onChange={handleSearchChange}
-        placeholder={suggestions.join(', ')}
-      />
-      {/* Bouton de recherche */}
-      <button type="submit" className="searchBtn">
-      <ImSearch />
-      </button>
-    </form>
+    <div>
+      <form className="searchForm" onSubmit={handleSearchSubmit}>
+        <input
+          type="text"
+          id="searchInput"
+          className="searchInput"
+          value={searchValue}
+          onChange={handleSearchChange}
+          placeholder={suggestions.join(', ')}
+        />
+        <button type="submit" className="searchBtn">
+          <ImSearch />
+        </button>
+      </form>
+
+      {/* Afficher les résultats de la recherche */}
+      {prodData.length > 0 && (
+        <div className="searchResults">
+          {prodData.map((product, index) => (
+            <div key={index} className="productItem">
+              <h3>{product.name}</h3>
+              <p>{product.description}</p>
+              <p>Prix: {product.price}€</p>
+              <img className='ImgSearch' src={`${img_url}${product.image}`} alt={product.name} />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
