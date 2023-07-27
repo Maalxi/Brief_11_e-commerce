d93d18be05b5dde65f1a7dee72c8fa666e928821
Guillaume <estrade.guillaume.simplon@gmail.com>
2023-07-27 15:30:34 +0200

    Validation Container Panier

--- front/src/app/panier/page.jsx
+++ front/src/app/panier/page.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Layout from "../Layout";
+import UserFormCart from "../components/UserFormCart/UserFormCart";
 
 
 export default function Panier() {
   return (
     <Layout>
+      <UserFormCart/>
     </Layout>
   );
 }
